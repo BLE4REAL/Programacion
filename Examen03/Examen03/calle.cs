2ce0875661f573495eba3928a14f21802d8a9fff
Juan Dario Rodas <jdrodas@hotmail.com>
2023-05-08 21:08:26 -0500

    Redefinición estructura del proyecto para permitir agregar pruebas unitarias

---
+++ Examen03/Examen03/calle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Examen03
+{
+    public class Calle
+    {
+        //atributos de la clase
+        private string deteriroro;
+        private int longitud;
+        private float tramo;
+
+        //constructor de la clase
+
+        public Calle()
+        {
+            longitud = 0;
+            tramo = 0;
+            deteriroro = string.Empty;
+        }
+
+        //Porpiedades de acceso de los atributos
+
+        public int Longitud
+        {
+            get { return longitud; }
+            set { longitud = value; }
+        }
+
+        public float Tramo
+        {
+            get { return tramo; }
+            set { tramo = value; }
+        }
+
+        public string Deterioro
+        {
+            get { return deteriroro; }
+            set { deteriroro = value; }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Examen No. 3 - Fundamentos de Programación
+
+## Pavimentación de Calles
+
+Realizado por:
+- Juan Esteban Araujo
+- Nicolás Correa Vásquez
+
