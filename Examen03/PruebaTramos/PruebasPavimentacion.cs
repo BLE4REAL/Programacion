339a4247ae0b8a2d844c6e4baef6817c79ef92b7
BLE4REAL <arajuanes405@gmail.com>
2023-05-10 09:52:31 -0500

    Sincronizada la version de .NET framework para todos los proyectos

--- Examen03/PruebaTramos/PruebasPavimentacion.cs
+++ Examen03/PruebaTramos/PruebasPavimentacion.cs
@@ -3,10 +3,10 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace PruebaTramos
 {
     [TestClass]
-    public class UnitTest1
+    public class PruebasPavimentacion
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PruebaLongitudPavimentada()
         {
         }
     }
